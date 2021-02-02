9e7f6b289d631a3148dc4f73b5346a5a28c3426d
d-vanos <danielle.vanos@unimelb.edu.au>
2021-02-02 20:44:33 +1000

    Create global.R

---
+++ App/global.R
@@ -0,0 +1,45 @@
+
+
+# -----------------------------------------------------------------
+#
+# This code loads all the relevant packages required by the app.
+# It also loads the elements and scripts required by the UI.
+#
+# ----------------------------------------------------------------- 
+
+
+
+
+
+
+# -----------------------------------------------------------------
+#
+#
+# Load libraries 
+# 
+#
+# -----------------------------------------------------------------
+
+# Shiny libraries
+library(shiny)
+library(shinydashboard)
+library(shinyBS)
+
+# Other relevant libraries
+library(tidyverse)
+library(MASS) # For random data generation with exact means 
+
+
+
+
+
+
+
+
+# -----------------------------------------------------------------
+#
+#
+# Load scripts for the UI 
+# 
+#
+# -----------------------------------------------------------------
