fc707df44b97425e6e1287b93b230873f163fdbc
xuejcheng <78767347+xuejcheng@users.noreply.github.com>
2021-02-16 14:36:22 +1100

    Update model_info.R

--- App/model_info.R
+++ App/model_info.R
@@ -14,18 +14,28 @@
 model_info <- "Information about the model to go here. For example, if it is categorical and has two groups, it is an ANOVA.
               If the outcome variable is categorical, it is likely a logistic regression.
 
-              This section can include info on how models relate to one another, and historical reasons for different names.
+This section can include info on how models relate to one another, and historical reasons for different names.
 - The name of the model (e.g., t-test, ANOVA)
 - What makes it that type of model (e.g., categorical, 2 categories)
 - Why (historically) it has been given the name that it has.
 - How it similar to/different from a linear model
 - Assumptions??"
 
-linear_regression <- HTML("Linear Regression: to complete")
-t_test <- HTML("T-Test: to complete")
-one_way_ANOVA <- HTML("One-way ANOVA: to complete")
-factorial_ANOVA <- HTML("Factorial ANOVA: to complete")
-logistic_regression <- HTML("Logistic Regression: to complete")
+linear_regression <- HTML("Linear Regression: ")
+t_test <- HTML("T-Test: A t-test
+               Beer test!")
+one_way_ANOVA <- HTML("One-way ANOVA: ANOVA stands for Analysis of Variance, and is a test used to analyse the the difference between means of two or more groups. 
+                      An ANOVA is used when you have a <b>categorical</b> independent variable, and one quantitative dependent variable. 
+                      One-way refers to when you have one indepenedent variable, and two-way when you have two independent variables.
+                      ")
+factorial_ANOVA <- HTML("Factorial ANOVA: A factorial ANOVA is ____
+                        It is used when you look at the effects of more than one factor together
+                        It gives us information about their dependence or independence in the same experiment.")
+logistic_regression <- HTML("Logistic Regression: This is also a type of regression analysis.
+With logistic regression
+                            It predicts a binary outcomes based on a set of independent variables.
+                            A binary outcome is when there are only two possible outcomes - ")
+# put in the other models!!
 other <- HTML("No description has been created for this type of model. Keep an eye out, as this might appear soon! ", 
               "&#x1F440") # this HTML entity creates an emoji hehe
 
