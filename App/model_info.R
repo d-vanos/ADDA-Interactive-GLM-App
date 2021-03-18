a16d9de1f0712157491e3b6acd798ca1cc74965c
xuejcheng <78767347+xuejcheng@users.noreply.github.com>
2021-03-18 14:28:19 +1100

    Update model_info.R

--- App/model_info.R
+++ App/model_info.R
@@ -21,12 +21,6 @@ This section can include info on how models relate to one another, and historica
 - How it similar to/different from a linear model
 - Assumptions??"
 
-
-linear_regression <- HTML("Linear Regression: Linear regression is a linear approach 
-                          to modelling the relationship between a scalar response and one (or more) dependent and independent  
-                          variables. When you have one independent variable, it is a simple linear regression. When you have more than one 
-                          independent variable, it is a multiple linear regression.")
-
 t_test <- HTML("<b>Model Type:</b> T-Test <br>
                 <b>Description:</b> A t-test is used to compare the means between two groups. Is is an application of the general linear model 
 as you can treat your groups (a binary variable), as a dummy variable and assign the value of 1 for group A, and 0 for group B. <br> 
@@ -41,19 +35,23 @@ one_way_ANOVA <- HTML("<b>Model Type:</b> One-way ANOVA <br>
                        <b>Description:</b> ANOVA stands for Analysis of Variance, and is a test used to analyse the the difference between means of two or more groups. 
                       An ANOVA is used when you have a <b>categorical</b> independent variable, and one quantitative dependent variable. 
                       One-way refers to when you have one independent variable, and two-way when you have two independent variables. <br>
-                      ")
+                      <b>History:</b> The ANOVA was developed by statistician Ronald Fisher who you may know for many other contributions to the statistics that we know today.
+                      His first application of the ANOVA was to analyse a huge amount of data collected about crops and their variation over the years.")
 
 linear_regression <- HTML("<b> Model Type: </b> Linear Regression <br> 
 <b> Description: </b> Linear regression is a linear approach to modelling the relationship between a two or more continuous variables.
 When you have one independent/predictor variable, it is a simple linear regression. When you have more than one independent variable, 
-it is a multiple linear regression.")
+it is a multiple linear regression.<br>
+<b> History:</b> The initial conceptualisation of linear regression happened in the late 1800s when Sir Francis Galton examined characteristics of the sweet pea plant.
+                          He had distributed seeds of the same weight to his friends and obtained the seeds from the next generation of the plants. His two-dimensional 
+                          plot of the sizes of daughter peas against the sizes of mother peas was an illustration of the foundations of regression.")
 
 
 factorial_ANOVA <- HTML("<b>Model Type:</b> Factorial ANOVA <br>
-                        <b>Description:</b> A factorial ANOVA is ____
-                        It is used when you look at the effects of more than one factor together
-                        It gives us information about their dependence or independence in the same experiment. <br>
-                        <b>History:</b> ")
+                        <b>Description:</b> A factorial ANOVA is used to determine whether two or more sets of groups (categorical) differ on a variable of interest, and whether there is an interaction 
+between the factors in the same experiment. This variable of interest should be continuous, normally distributed, and have a similar spread across your groups. The two-way ANOVA 
+is a special case of the Factorial ANOVA. The simplest factorial design contains 2 levels for each of 2 factors: a 2 x 2 design.<br>
+                        <b>History:</b> Factorial designs in experiments started being used in the 1800s by the Rothamsted Experimental Station (specifically Lawes and Gilbert).")
 
 moderation <- HTML("<b>Model Type:</b> Moderation <br>
                    <b> Description:</b>  <br>
